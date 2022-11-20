9b506978c37a799340f7075b1015f81c879604e5
Surya Kolli <chandrasekhar.kohli@gmail.com>
2022-11-20 15:01:18 -0500

    added files

---
+++ src/mergsort/Main.java
@@ -0,0 +1,17 @@
+package mergsort;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Main {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        List<Integer> arr = List.of(9,7,8,6,1,2,4,3,5);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<List<Integer>> futureSortedList = executorService.submit(new Sorter(arr));
+        List<Integer> sortedList = futureSortedList.get();
+        System.out.println(sortedList);
+    }
+}
---
+++ src/mergsort/Sorter.java
@@ -0,0 +1,67 @@
+package mergsort;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Sorter implements Callable<List<Integer>> {
+    private List<Integer> list;
+
+    Sorter(List<Integer> l) {
+        this.list = l;
+    }
+
+    @Override
+    public List<Integer> call() throws Exception {
+        if(list.size() <= 1) return list;
+
+        int mid = list.size() / 2;
+        List<Integer> left = new ArrayList<>();
+        for(int i = 0; i < mid; i++) {
+            left.add(list.get(i));
+        }
+        List<Integer> right = new ArrayList<>();
+        for(int i = mid; i < list.size(); i++) {
+            right.add(list.get(i));
+        }
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<List<Integer>> futureSortedLeft = executorService.submit(new Sorter(left));
+        Future<List<Integer>> futureSortedRight = executorService.submit(new Sorter(right));
+        List<Integer> sortedLeft = futureSortedLeft.get();
+        List<Integer> sortedRight = futureSortedRight.get();
+
+        return merge(sortedLeft, sortedRight);
+    }
+
+    private List<Integer> merge (List<Integer> left, List<Integer> right) {
+        List<Integer> finalList = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+
+        while (i < left.size() && j < right.size()) {
+            if (left.get(i) < right.get(j)) {
+                finalList.add(left.get(i));
+                i++;
+            } else {
+                finalList.add(right.get(j));
+                j++;
+            }
+        }
+
+        while (i < left.size()) {
+            finalList.add(left.get(i));
+            i++;
+        }
+
+        while (j < right.size()) {
+            finalList.add(right.get(j));
+            j++;
+        }
+
+        return finalList;
+    }
+}
