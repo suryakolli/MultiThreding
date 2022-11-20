1223cfd62550caa5c73134f2f95e5a2312fd547e
Surya Kolli <chandrasekhar.kohli@gmail.com>
2022-11-20 15:20:48 -0500

    added shutdown and termination for execution service with a little formatting

--- src/mergsort/Main.java
+++ src/mergsort/Main.java
@@ -1,17 +1,21 @@
 package mergsort;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         List<Integer> arr = List.of(9,7,8,6,1,2,4,3,5,10);
+
         ExecutorService executorService = Executors.newCachedThreadPool();
+
         Future<List<Integer>> futureSortedList = executorService.submit(new Sorter(arr));
+
         List<Integer> sortedList = futureSortedList.get();
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MILLISECONDS);
+
         System.out.println(sortedList);
     }
 }
--- src/mergsort/Sorter.java
+++ src/mergsort/Sorter.java
@@ -2,13 +2,10 @@ package mergsort;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 public class Sorter implements Callable<List<Integer>> {
-    private List<Integer> list;
+    private final List<Integer> list;
 
     Sorter(List<Integer> l) {
         this.list = l;
@@ -19,20 +16,27 @@ public class Sorter implements Callable<List<Integer>> {
         if(list.size() <= 1) return list;
 
         int mid = list.size() / 2;
+
         List<Integer> left = new ArrayList<>();
         for(int i = 0; i < mid; i++) {
             left.add(list.get(i));
         }
+
         List<Integer> right = new ArrayList<>();
         for(int i = mid; i < list.size(); i++) {
             right.add(list.get(i));
         }
 
         ExecutorService executorService = Executors.newCachedThreadPool();
+
         Future<List<Integer>> futureSortedLeft = executorService.submit(new Sorter(left));
         Future<List<Integer>> futureSortedRight = executorService.submit(new Sorter(right));
+
         List<Integer> sortedLeft = futureSortedLeft.get();
         List<Integer> sortedRight = futureSortedRight.get();
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MILLISECONDS);
 
         return merge(sortedLeft, sortedRight);
     }
