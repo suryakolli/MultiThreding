144c5cdb181489b7f3309d02bf290708f004ae0a
Surya Kolli <chandrasekhar.kohli@gmail.com>
2022-11-20 15:15:34 -0500

    added element 10 in the list that needs sorting

--- src/mergsort/Main.java
+++ src/mergsort/Main.java
@@ -8,7 +8,7 @@ import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        List<Integer> arr = List.of(9,7,8,6,1,2,4,3,5);
+        List<Integer> arr = List.of(9,7,8,6,1,2,4,3,5,10);
         ExecutorService executorService = Executors.newCachedThreadPool();
         Future<List<Integer>> futureSortedList = executorService.submit(new Sorter(arr));
         List<Integer> sortedList = futureSortedList.get();
